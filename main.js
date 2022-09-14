851f8e8a73434153f9ba0d2bcf38af1886d30f74
johinner <johimadacor@live.com>
2022-09-13 21:49:02 -0500

    Cómo obtener datos de un archivo JSON JavaScript

---
+++ main.js
@@ -0,0 +1,40 @@
+import data from './data.json' assert{type: 'json'};
+
+const chartBarConteiner = document.querySelector('.chart__bars--container');
+const chartTotal = document.getElementById('chart__total');
+
+let valorMayor = 0;
+const maximoPixel = 200;
+data.forEach(Element => {
+    if(Element.amount > valorMayor) valorMayor = Element.amount; 
+});
+
+// Regla de 3
+// valorMayor -> 200px
+// valorN -> x
+// x = (valorN * 200px)/valorMayor
+
+let totalAmout = 0;
+data.forEach(Element => {
+    let height =  (Element.amount * maximoPixel) / valorMayor;
+    chartBarConteiner.innerHTML += `<div style="height: ${height}px ;" class="chart__bar" >
+    <div class="chart__bar--label">$<span>${Element.amount}</span></div>
+    <div class="chart__bar--day">${Element.day}</div>
+  </div>`
+  totalAmout += Element.amount
+});
+chartTotal.textContent = totalAmout;
+
+let bars = document.querySelectorAll('.chart__bar .chart__bar--label span');
+bars = [...bars];
+
+
+let barsMayor;
+bars.forEach(Element => {
+     if(parseFloat(Element.textContent) === valorMayor){
+        barsMayor = Element.parentNode.parentNode;
+     }
+})
+
+//barsMayor.setAttribute("style", "background-color: hsl(186, 34%, 60%);");
+barsMayor.style.background = 'hsl(186, 34%, 60%)'
